6beb3ec97ca1015941f8e4570b814d6b8cecea2a
Dokuparthi Nilesh <110710201+Nilesh-27@users.noreply.github.com>
2022-10-16 08:30:04 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,33 @@
+
+import './App.css';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
+import About from './components/About';
+import Services from './components/Services';
+import Contact from './components/Contact';
+import {Route, Routes} from 'react-router-dom'
+import Login from './components/Login';
+import Register from './components/Register';
+import Overview from './components/Overview';
+import Logout from './components/Logout';
+import Protectedroute from './Projectedroute';
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+    <Routes>
+      <Route exact path="/" element={<Home/>}/>
+      <Route exact path="/about" element={<About/>}/>
+      <Route exact path="/services" element={<Services/>}/>
+      <Route exact path="/contact" element={<Contact/>}/>
+      <Route exact path="/login" element={<Login/>}/>
+    <Route exact path="/register" element={<Register/>}/>
+    <Route exact path="/overview" element={<Overview/>}/>
+    <Route exact path="/logout" element={<Logout/>}/>
+    </Routes>
+    </>
+  );
+}
+
+export default App;
